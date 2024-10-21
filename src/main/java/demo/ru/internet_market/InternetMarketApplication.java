812257de3320ec7665a4397a7b9861966082bf14
Elvina <elvina.smirnova.205@mail.ru>
2024-10-21 13:52:33 +0300

    Generate project

---
+++ src/main/java/demo/ru/internet_market/InternetMarketApplication.java
@@ -0,0 +1,13 @@
+package demo.ru.internet_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/ru/internet_market/InternetMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ru.internet_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
